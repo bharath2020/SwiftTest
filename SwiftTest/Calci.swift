dc460bc275436521a786a7438c462afbda6cddf2
bharath2020 <bbooshan@atlassian.com>
2017-09-01 08:54:51 -0700

    first commit

---
+++ SwiftTest/Calci.swift
@@ -0,0 +1,23 @@
+//
+//  Calci.swift
+//  SwiftTest
+//
+//  Created by Bharath Booshan on 9/1/17.
+//  Copyright © 2017 Atlassian. All rights reserved.
+//
+
+import Foundation
+
+public class Calci {
+    static func display() {
+        print("Hello Calci");
+    }
+
+    public init() {
+
+    }
+
+    func display1() {
+        print("Hello Calci - 1");
+    }
+}
---
+++ SwiftTest/SwiftTest.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTest.h
+//  SwiftTest
+//
+//  Created by Bharath Booshan on 9/1/17.
+//  Copyright © 2017 Atlassian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTest.
+FOUNDATION_EXPORT double SwiftTestVersionNumber;
+
+//! Project version string for SwiftTest.
+FOUNDATION_EXPORT const unsigned char SwiftTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTest/PublicHeader.h>
+
+
